e199a8460bda7230e0ed5c586debc7025b6f8e83
manisharigala <arigalamanish@gmail.com>
2021-05-30 16:09:00 -0700

    added write service

--- write/app.py
+++ write/app.py
@@ -1,4 +1,4 @@
-import pika, sys, os,json
+import pika, sys, os,json,requests
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -8,9 +8,66 @@ def main():
 
     def callback(ch, method, properties, body):
 
+
+        #parsing message request
         req=json.loads(body)
-        print("***")
-        print(req)
+        
+ 
+
+        #Fetching the schema
+        coreName="core1"
+        resp = requests.get("http://localhost:8983/solr/core1/schema/fields?wt=json")
+        
+
+        # #Fetching the fields
+
+        fields = json.loads(resp.text)['fields']
+        # print(fields)
+ 
+
+        #checking out what fields to create
+        toCreate=[]
+        existingFields=set()
+        for i in fields:
+            if i['name'] in req:
+                existingFields.add(i['name'])
+        for i in req:
+            if i not in existingFields:
+                curField={}
+                curField["name"]=str(i)
+                curField["type"]="string"
+                curField["indexed"]=True
+                curField["stored"]=True
+                toCreate.append(curField)
+        
+        print(toCreate)
+        # #creating the fields
+        data = {
+        "add-field": toCreate
+            }
+        
+        # print(data)
+        if len(toCreate)!=0:
+            res=requests.post("http://localhost:8983/solr/core1/schema",str(data))
+            print(res)
+        
+
+
+        #inserting documents
+
+        headers={"Content-Type":"application/json"}
+
+
+        docs=[]
+        for i in req:
+            docs.append(i)
+        print(docs)
+        # requests.post("http://localhost:8983/solr/${coreName}/update?commit=true",docs,headers=headers)
+
+        
+        # print(toCreate)
+
+
         # print(req)
         # print(" [x] Received %r" % body)
 
